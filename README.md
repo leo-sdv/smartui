3d7ba7a1c383bbe966dcb28b82d06fa4755a4e62
mayage <mayage@saicmotor.com>
2016-08-04 11:31:45 +0800

    modify for little comments

--- README.md
+++ README.md
@@ -4,9 +4,15 @@
 ##complie and run:
 	gcc hello.c -lglut -lGL
 
+	gcc hello.c -lglut -lGL -lGLU
 
 http://openglgui.sourceforge.net/
 
 http://www.cppblog.com/doing5552/archive/2009/01/08/71532.html
 
 http://blog.csdn.net/iduosi/article/details/7835624# smartui
+
+## an excellent reference web-page
+http://glprogramming.com/red/index.html
+
+## Q&A
--- sample/circle.c
+++ sample/circle.c
@@ -1,7 +1,7 @@
 #include <GL/glut.h>
 #include <math.h>
 
-const int n = 50;
+const int n = 25;
 const GLfloat R = 0.5f;
 const GLfloat Pi = 3.1415926536f;
 
@@ -9,6 +9,22 @@ void myDisplay()
 {
     int i;
     glClear(GL_COLOR_BUFFER_BIT);
+
+    // 点，线 消除锯齿
+    // glEnable(GL_POINT_SMOOTH);
+    // glHint(GL_POINT_SMOOTH, GL_NICEST);
+
+    // glEnable(GL_LINE_SMOOTH);
+    // glHint(GL_LINE_SMOOTH_HINT, GL_NICEST);
+
+    // glEnable(GL_POLYGON_SMOOTH_HINT);
+    // glHint(GL_POLYGON_SMOOTH_HINT, GL_NICEST);
+
+    // 采用了多重采样，点线设置将会忽略掉
+    
+    glEnable(GL_MULTISAMPLE);
+    glEnable(GL_BLEND); //enable
+
     glBegin(GL_POLYGON);
     for(i = 0; i < n; i++)
     {
@@ -16,6 +32,9 @@ void myDisplay()
     }
     glEnd();
     glFlush();
+
+    glDisable(GL_BLEND);// 画完后disable掉
+    glDisable(GL_MULTISAMPLE);
 }
 
 
@@ -24,7 +43,7 @@ int main(int argc, char *argv[])
 {
     glutInit(&argc, argv);
 
-    glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
+    glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE | GLUT_MULTISAMPLE);
 
     glutInitWindowPosition(100, 100);
 
